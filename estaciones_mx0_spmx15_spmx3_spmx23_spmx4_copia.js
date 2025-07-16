6cb894c96c4e1adb890a5c4f0cc807cb62327ef9
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-16 17:19:43 -0600

    Add files via upload

---
+++ estaciones_mx0_spmx15_spmx3_spmx23_spmx4_copia.js
@@ -0,0 +1,11 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_spmx15_spmx3_spmx23_spmx4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "MexiquenseParks, 54900, De las Torres 191, Sin Nombre, 54910 Fuentes del Valle, Méx.", "description": "MX0", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1476988, 19.6397536, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Mueblería de La Romana, Calle Cuauhtémoc 202, Hab la Romana, 54000 Tlalnepantla, Méx.", "description": "SPMX15", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.190283, 19.5407017, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Av Acueducto de Tenayuca 20, San José de la Escalera, 07630 Ciudad de México, CDMX", "description": "SPMX3", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1667806, 19.5287616, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Hda. de La Carbonera 4, MZ 012, Bosques de Echegaray, 53310 Naucalpan de Juárez, Méx.", "description": "SPMX23", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.2309629, 19.4871487, 0.0 ] } }
+]
+}
